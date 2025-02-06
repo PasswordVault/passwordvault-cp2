007ea959873f1b4320010b1da999855e5dd84aa2
Olav Schettler <olav@schettler.net>
2025-02-06 15:41:27 +0100

    Changed database format to save space: Separated keys from passwords

--- mkdb.py
+++ mkdb.py
@@ -12,7 +12,7 @@ now = datetime.datetime.now().replace(microsecond=0).isoformat(' ')
 
 header = f'''
 # Passwords ({now})
-entries = {{ 
+entries = {{
 '''
 
 footer = '''
@@ -20,18 +20,21 @@ footer = '''
 '''
 
 def print_entries():
-    with open('passwd.txt', 'rt') as f:
-        print("Reading...", file=sys.stderr)
-        while True:
-            l = f.readline()
-            if not l:
-                break
-            name, passwd = l.split('\t', 1)
-            passwd = passwd.rstrip()
-
-            enc = xxtea.encryptToBase64(passwd, key)
-            print(f'  "{name}": "{enc}",')
-        print("Done.", file=sys.stderr)
+    with open('passwd.txt') as inp:
+        with open('pvalues.txt', 'w') as values:
+            pos = 0
+            print("Reading...", file=sys.stderr)
+            while True:
+                l = inp.readline()
+                if not l:
+                    break
+                name, passwd = l.split('\t', 1)
+                passwd = passwd.rstrip()
+                enc = xxtea.encryptToBase64(passwd, key)
+                print(f'  "{name}": ({pos},{len(enc)}),')
+                values.write(enc)
+                pos += len(enc)
+    print("Done.", file=sys.stderr)
 
 if __name__ == '__main__':
     print(header)
